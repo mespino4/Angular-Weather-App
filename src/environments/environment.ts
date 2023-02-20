8a6817d3701b42f15edecfe03fa8800a31e8e51c
Mely Espino <90160548+melyespinofl96@users.noreply.github.com>
2023-02-20 15:08:32 -0600

    update eme

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/',
+
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '74c9549321msh7331cf855c97183p170f6ejsn3ed947922284'
+
+
+};
