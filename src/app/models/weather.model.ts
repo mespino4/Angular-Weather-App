9970e5a070ca3e6f63d977d54336349349962f36
Mely Espino <90160548+melyespinofl96@users.noreply.github.com>
2023-02-23 22:32:12 -0600

    change weather api

--- src/app/models/weather.model.ts
+++ src/app/models/weather.model.ts
@@ -1,4 +1,39 @@
 export interface weatherData {
+  "data": {
+    "time": "2023-02-24T03:07:00Z",
+    "values": {
+      "cloudBase": 0.23,
+      "cloudCeiling": 0.23,
+      "cloudCover": 99,
+      "dewPoint": 21.5,
+      "freezingRainIntensity": 0,
+      "humidity": 90,
+      "precipitationProbability": 0,
+      "pressureSurfaceLevel": 1015.35,
+      "rainIntensity": 0,
+      "sleetIntensity": 0,
+      "snowIntensity": 0,
+      "temperature": 23.63,
+      "temperatureApparent": 23.63,
+      "uvHealthConcern": 0,
+      "uvIndex": 0,
+      "visibility": 13.75,
+      "weatherCode": 1001,
+      "windDirection": 138.63,
+      "windGust": 4,
+      "windSpeed": 1.19
+    }
+  },
+  "location": {
+    "lat": 29.75893783569336,
+    "lon": -95.3676986694336,
+    "name": "Houston, Harris County, Texas, United States",
+    "type": "administrative"
+  }
+}
+
+/*
+export interface weatherData {
     coord: Coord
     weather: Weather[]
     base: string
@@ -52,4 +87,4 @@ export interface weatherData {
     sunrise: number
     sunset: number
   }
-  
+  */
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { weatherData } from '../models/weather.model';
 import { Observable } from 'rxjs';
+import { __param } from 'tslib';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,22 @@ export class WeatherService {
 
   constructor(private http:HttpClient) { }
 
+  /*
   getWeatherData(cityName: string): Observable<weatherData>{
-    return this.http.get<weatherData>('https://open-weather13.p.rapidapi.com/city/' + cityName, {
+    return this.http.get<weatherData>('https://tomorrow-io1.p.rapidapi.com/timelines?', {
       headers: new HttpHeaders()
-      .set('X-RapidAPI-Host', 'open-weather13.p.rapidapi.com')
+      .set('X-RapidAPI-Host', 'tomorrow-io1.p.rapidapi.com')
       .set('X-RapidAPI-Key', '74c9549321msh7331cf855c97183p170f6ejsn3ed947922284'),
+      params: new HttpParams()
+      .set('location', '%7B%7B' + cityName + '%7D%7D&')
     })
   }
+}
+*/
+
+  getWeatherData(cityName: string): Observable<weatherData>{
+    return this.http.get<weatherData>
+    ('https://api.tomorrow.io/v4/weather/realtime?location='
+    + cityName + '&apikey=k7iIl3hRtrz6YVQnMgqdhsouSZoeKG3O', {})
+    }
 }
